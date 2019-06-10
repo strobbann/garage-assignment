92d3264ab77a112d738b559167ed8fad79f70f87
strobbann <strobbann@gmail.com>
2019-06-10 09:09:39 +0200

    Added to test builder

--- src/test/java/VehicleTest.java
+++ src/test/java/VehicleTest.java
@@ -7,14 +7,17 @@ public class VehicleTest {
     @Test
     public void park() {
         Garage garage = new Garage();
-        Vehicle testVehicle1 = Motorcycle.builder().withRegistrationNumber("RED987").build();
-        Vehicle testVehicle2 = Bus.builder().withRegistrationNumber("POD783").build();
+        Vehicle testVehicle1 = Motorcycle.builder().withRegistrationNumber("RED987").withFuelType("Diesel").build();
+        Vehicle testVehicle2 = Bus.builder().withNumberOfSeats(80).withRegistrationNumber("POD783").build();
+        Vehicle testVehcile3 = Car.builder().withRegistrationNumber("IUO343").withCylinderVolume(1888).build();
 
         testVehicle1.park(garage);
         testVehicle2.park(garage);
+        testVehcile3.park(garage);
 
         assertEquals(testVehicle1, garage.getVehicleByRegistrationNumber("RED987"));
         assertEquals(testVehicle2, garage.getVehicleByRegistrationNumber("POD783"));
+        assertEquals(testVehcile3, garage.getVehicleByRegistrationNumber("IUO343"));
         System.out.println(garage);
 
     }
